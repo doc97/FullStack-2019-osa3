f43a2c3c989caddbc0f83219571de52d37b3b01f
doc97 <daniel.riissanen@gmail.com>
2019-02-08 18:02:57 +0200

    Add frontend

---
+++ build/precache-manifest.88345ff68ee88f3b55738bd748ed2c85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afb5f5d057199fa19f9c",
+    "url": "/static/js/main.afb5f5d0.chunk.js"
+  },
+  {
+    "revision": "f8a75abd891cc76325d3",
+    "url": "/static/js/1.f8a75abd.chunk.js"
+  },
+  {
+    "revision": "afb5f5d057199fa19f9c",
+    "url": "/static/css/main.e776bd33.chunk.css"
+  },
+  {
+    "revision": "c2e2059545f48ad096811c8b576be82b",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 /* Morgan configuration */
@@ -9,7 +10,9 @@ morgan.token('body', (req, res) => {
 })
 
 /* Middleware */
+app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
 app.use(morgan(
   [
     ':method',
@@ -116,5 +119,5 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 /* Server start */
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
